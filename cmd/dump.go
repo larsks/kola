1dc64799e44e4b28527ef96844550914029c169a
Lars Kellogg-Stedman <lars@oddbit.com>
2022-12-01 14:15:30 -0500

    Add dump command

---
+++ cmd/dump.go
@@ -0,0 +1,80 @@
+/*
+Copyright © 2022 Lars Kellogg-Stedman <lars@oddbit.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"errors"
+	"kola/client"
+	"kola/packagemanager"
+	"os"
+
+	operatorsv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
+	operators "github.com/operator-framework/operator-lifecycle-manager/pkg/package-server/apis/operators/v1"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"k8s.io/kubectl/pkg/scheme"
+)
+
+type (
+	DumpFlags struct {
+	}
+)
+
+var dumpFlags = DumpFlags{}
+
+// dumpCmd represents the dump command
+var dumpCmd = &cobra.Command{
+	Use:   "dump",
+	Short: "Dump details about a package",
+	Run:   runDump,
+}
+
+func init() {
+	rootCmd.AddCommand(dumpCmd)
+	AddFlagsFromSpec(dumpCmd, &dumpFlags, false)
+}
+
+func runDump(cmd *cobra.Command, args []string) {
+	if len(args) != 1 {
+		panic(errors.New("dump requires a single package name"))
+	}
+
+	clientset, err := client.GetClient(rootFlags.Kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	pm := packagemanager.NewPackageManager(clientset)
+	pkg, err := pm.GetPackageManifest(args[0])
+	if err != nil {
+		panic(err)
+	}
+
+	if err := dumpPackage(pkg); err != nil {
+		panic(err)
+	}
+}
+
+func dumpPackage(pkg *operators.PackageManifest) error {
+	operatorsv1alpha1.AddToScheme(scheme.Scheme)
+	operators.AddToScheme(scheme.Scheme)
+	s := json.NewYAMLSerializer(json.DefaultMetaFactory, scheme.Scheme,
+		scheme.Scheme)
+
+	err := s.Encode(pkg, os.Stdout)
+	return err
+}
