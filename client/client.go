79efdcef226095a5237106f86ddd937ab60f0320
Lars Kellogg-Stedman <lars@oddbit.com>
2022-12-05 23:17:46 -0500

    Add context to kubeconfig related errors

--- client/client.go
+++ client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"fmt"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	restclient "k8s.io/client-go/rest"
@@ -35,12 +37,12 @@ func BuildConfigFromFlags(masterUrl, kubeconfigPath string) (*restclient.Config,
 func GetClient(kubeconfig string) (*rest.Config, *kubernetes.Clientset, error) {
 	config, err := BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("failed to build Kuberbetes config: %w", err)
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, fmt.Errorf("failed to create Kubernetes client: %w", err)
 	}
 
 	return config, clientset, nil
